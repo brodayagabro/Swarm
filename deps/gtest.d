374b176a44c90fd69ac36073e2f7db1de49af15e
nickolai <brodayagbro1@gmail.com>
2023-11-28 01:42:50 +0300

    3

---
+++ deps/gtest.d
@@ -0,0 +1,7 @@
+gtest.o: src/gtest.cpp src/../hdr/Robot.h src/../hdr/WorkingRobot.h \
+ src/../hdr/Robot.h src/../hdr/exceptions.h src/../hdr/CommanderRobot.h \
+ src/../hdr/WorkingRobot.h src/../hdr/Swarm.h src/../hdr/CommanderRobot.h \
+ src/../gtestd/RobotTest.hpp src/../gtestd/../hdr/Robot.h \
+ src/../gtestd/WorkingRobotTest.hpp src/../gtestd/../hdr/WorkingRobot.h \
+ src/../gtestd/../hdr/CommanderRobot.h \
+ src/../gtestd/CommanderRobotTest.hpp src/../gtestd/SwarmTest.hpp
